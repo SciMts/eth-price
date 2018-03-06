22dd8d792a7d4e4b5436ecf54c62123cebf1ff73
Jonny Burger <jonathanburger11@gmail.com>
2018-03-06 14:24:12 +0100

    testnet blocks

---
+++ test/eth-block-no-testnet.js
@@ -0,0 +1,22 @@
+import test from 'ava';
+import {getFromEtherscan, getFromInfura} from '../lib/get-eth-block';
+
+test.beforeEach(() => {
+	process.env.TESTNET = true;
+});
+
+test.afterEach(() => {
+	process.env.TESTNET = false;
+});
+
+test('Etherscan', async t => {
+	const etherscan = await getFromEtherscan();
+	t.true(etherscan > 1884603);
+	t.true(etherscan < 50000000);
+});
+
+test('Gastracker', async t => {
+	const gastracker = await getFromInfura();
+	t.true(gastracker > 1884603);
+	t.true(gastracker < 50000000);
+});
